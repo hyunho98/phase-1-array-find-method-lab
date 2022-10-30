25ff470b96e299c4dada5773919f178dc425bd38
Alex Lee <hyunho98@gmail.com>
2022-10-30 12:35:25 -0400

    commit tree 2187df37ba5993de09729b3dc9182a21e31ef3bb

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(array) {
+    let win;
+    win = array.find(element => element.result === "W");
+
+    if (win)
+        return win.year;
+    else
+        return undefined;
+}   
